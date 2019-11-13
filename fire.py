9d4899f0bc078c39ee50f896191434ef614f12b5
Conway Hsieh <cchsieh@gammaq.com>
2019-11-13 13:56:43 -0800

    added feature layer separating income

--- fire.py
+++ fire.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 #from matplotlib import cm
 import folium
+from folium.map import FeatureGroup
 import branca
 import branca.colormap as cm
 import os
@@ -57,18 +58,28 @@ aff = combo['estCustAffected'].astype(int).values
 inc = combo['Per capita income'].astype(int).values
 #print(inc)
 
-mininc = inc.min()
-maxinc = inc.max()
+thousand = 1000
+mininc = inc.min()/thousand
+maxinc = inc.max()/thousand
 
 start_lat = 39
 start_lon = -121.5
+
+income_steps = [0, 30000, 50000, 80000, 100000, 200000]
+feature_group1 = FeatureGroup(name='Less than $30,000 Per Capita Income')
+feature_group2 = FeatureGroup(name='$30,000-$50,000 Per Capita Income')
+feature_group3 = FeatureGroup(name='$50,000-$80,000 Per Capita Income')
+feature_group4 = FeatureGroup(name='$80,000-$100,000 Per Capita Income')
+feature_group5 = FeatureGroup(name='More than $100,000 Per Capita Income')
+
+feat_list = [feature_group1, feature_group2, feature_group3, feature_group4, feature_group5]
 
 folium_map = folium.Map(location=[start_lat, start_lon], 
 						zoom_start = 8)
 cmap = cm.LinearColormap(colors=['red', 'yellow'], index=[mininc, maxinc],
     vmin=mininc, vmax=maxinc)
 cmap = cmap.to_step(n=12, method='log', round_method='log10')
-cmap.caption = 'Per Capita Income in Dollars ($)'
+cmap.caption = 'Per Capita Income in Thousands of Dollars ($1,000)'
 
 for index, row in combo.iterrows():
     popup_text = """Place: {}<br> 
@@ -78,11 +89,19 @@ for index, row in combo.iterrows():
                                    int(row['estCustAffected']),
                                    row['Per capita income'])
 
+    income = int(row['Per capita income'])
+    for i in range(len(feat_list)):
+        if income in range(income_steps[i], income_steps[i+1]):
+            f = feat_list[i]
+
     folium.CircleMarker(location=(row['latitude'], row['longitude']),
         radius=row['estCustAffected']/700,
-        color=cmap(int(row['Per capita income'])),
+        color=cmap(income/thousand),
         popup=popup_text,
-        fill=True).add_to(folium_map)
+        fill=True).add_to(f)
 
+for feat_group in feat_list:
+    folium_map.add_child(feat_group)
 folium_map.add_child(cmap)
+folium.LayerControl().add_to(folium_map)
 folium_map.save('./docs/my_map.html')
